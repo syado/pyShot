e6bec2276ecb8768fc94e58eeee86e7671895b38
syado <doi.sinya122@gmail.com>
2020-01-10 18:44:27 +0900

    修正

--- server/upload.py
+++ server/upload.py
@@ -1,13 +1,14 @@
-#!/usr/bin/python3.7
+#!/usr/bin/python3
 # -*- coding: utf-8 -*-
 import cgi
 import hashlib
 import time
 import os
 # import dbm.gnu as gdbm
-ip="127.0.0.1"
+print("Content-Type: text/html")
+print()
+ip="192.168.0.10"
 form = cgi.FieldStorage()
-
 id = form["id"].value
 imagedata = form["imagedata"].value
 hash = hashlib.md5(imagedata).hexdigest()
@@ -20,10 +21,10 @@ if not id or id == "":
 # dbm[hash] = id
 # dbm.close
 
-with open(f"{hash}.png", mode='wb') as f:
+with open("uploads/"+hash+".png", mode='wb') as f:
     f.write(imagedata)
 
 headers = {}
 if create_newid:
     print({"X-Gyazo-Id":id})
-    print(f"http://{ip}/{hash}.png")
+print("http://"+ip+"/uploads/"+hash+".png")
